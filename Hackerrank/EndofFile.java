e0d1b74bd882cc4bc21e22b66e8fa4f225e3622a
Nazneen Nahar <nazneen.nahar145@gmail.com>
2023-01-21 11:12:39 +0600

    8

---
+++ Hackerrank/EndofFile.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i=0;
+        while(scan.hasNextLine()){
+        String str=scan.nextLine();
+            if (str.contains("end-of-file")){
+                i++;
+                System.out.println(i+" "+str);
+                break;
+            }else{
+                i++;
+                System.out.println(i+" " +str);       
+            }
+        }
+    }
+}
