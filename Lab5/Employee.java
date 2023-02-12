709b7205ea35077c512fef2bc91a834c55c9abc2
Nazneen Nahar <nazneen.nahar145@gmail.com>
2023-02-12 21:38:15 +0600

    5

---
+++ Lab5/Employee.java
@@ -0,0 +1,23 @@
+public class Employee {
+
+	public String name ;
+	public String id ;
+	public String designation ; 
+	public double salary ; 
+	public Employee (String name , String id , String designation , double salary) {
+		this.name = name ;  
+		this.id= id ;
+		this.designation = designation ; 
+		this.salary = salary ;
+		
+	}
+	void updateSalary(double newSal) {
+		salary = newSal ; 
+	}
+	 double getSalary() {
+		return salary ; 
+	}
+	 void display() {
+		 System.out.println("Name: "+name+", Id: "+id+", Designation: "+designation+", Salary: "+salary);
+	 }
+}
