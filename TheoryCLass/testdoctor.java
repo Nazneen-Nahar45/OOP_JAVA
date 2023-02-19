c29acd1ab0de3b89cd37f8c1c0866e3196c946b6
Nazneen Nahar <nazneen.nahar145@gmail.com>
2023-02-19 23:01:48 +0600

    5

---
+++ TheoryCLass/testdoctor.java
@@ -0,0 +1,10 @@
+public class TestDoctor {
+	public static void main(String [] args) {
+		Doctor mydoctor = new Doctor("Nazneen" ,"JAMAI Specialist",2);
+		mydoctor.prescribe("Rakin" , "Ace");
+		mydoctor.display();
+		mydoctor.getRank();
+		
+	}
+
+}
